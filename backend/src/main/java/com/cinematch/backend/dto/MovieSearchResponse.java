29bdd85e5993b07bc56b45e454a7ea270922347a
Pasxosdam <pasxosdam@gmail.com>
2025-11-20 19:37:47 +0200

    fix TmdbService (#42)

--- backend/src/main/java/com/cinematch/backend/dto/MovieSearchResponse.java
+++ backend/src/main/java/com/cinematch/backend/dto/MovieSearchResponse.java
@@ -1,12 +1,19 @@
 package com.cinematch.backend.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import java.util.List;
 
 @Data
 public class MovieSearchResponse {
+
     private int page;
+
+    @JsonProperty("total_pages")
     private int totalPages;
+
+    @JsonProperty("total_results")
     private int totalResults;
+
     private List<MovieResultDto> results;
 }
