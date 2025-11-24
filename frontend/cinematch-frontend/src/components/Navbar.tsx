003116272e8043ae6247fed188a7ae5631ec24d9
KostasMartzoukos <martzkostas@gmail.com>
2025-11-25 00:08:27 +0200

    feat:US25 Login Logout register (#72)

--- frontend/cinematch-frontend/src/components/Navbar.tsx
+++ frontend/cinematch-frontend/src/components/Navbar.tsx
@@ -1,6 +1,16 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import type React from "react";
+import { logoutUser } from "../utils/auth";
 
 export default function Navbar() {
+      const navigate = useNavigate();
+
+      const isLoggedIn = !!localStorage.getItem("token");
+
+      function handleLogout() {
+        logoutUser();       // καθαρίζει token + user
+        navigate("/");      // γυρνάει στην αρχική
+      }
   return (
     <nav
       style={{
@@ -24,12 +34,35 @@ export default function Navbar() {
       <Link style={linkStyle} to="/quiz">Quiz</Link>
       <Link style={linkStyle} to="/profile">Profile</Link>
 
-      <div style={{ marginLeft: "auto" }}>
-        <Link style={linkStyle} to="/login">Login / Logout</Link>
-      </div>
-    </nav>
-  );
-}
+       {/* Δεξιά πλευρά */}
+            <div style={{ marginLeft: "auto", display: "flex", gap: "15px", paddingRight: "40px" }}>
+              {!isLoggedIn ? (
+                <>
+                  <Link style={linkStyle} to="/login">
+                    Login
+                  </Link>
+                  <Link style={linkStyle} to="/register">
+                    Register
+                  </Link>
+                </>
+                ) : (
+                <button
+                  onClick={handleLogout}
+                  style={{
+                    ...linkStyle,
+                    backgroundColor: "transparent",
+                    border: "none",
+                    cursor: "pointer",
+                    padding: 0,
+                  }}
+                >
+                  Logout
+                </button>
+              )}
+            </div>
+          </nav>
+        );
+      }
 
 const linkStyle: React.CSSProperties = {
   color: "white",
